96859457f297799fe873193938220b3eb3d0fb30
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-22 19:30:37 +0300

    complete styling cards with csstailwind

--- group-3-project/src/components/productList.jsx
+++ group-3-project/src/components/productList.jsx
@@ -6,8 +6,9 @@ import ProductCard from './ProductCard';
 function ProductList ({jewelries}) {
     // console.log(jewelries) //confirm if propr has contentn
     return (
-        <div>ALL JEWELRY
-            <div>
+        <div>
+            <h2 className=''>ALL JEWELRY</h2>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
             {jewelries.map((jewelry)=> (
                 <ProductCard key={jewelry.id} 
                 name={jewelry.name}
@@ -15,7 +16,6 @@ function ProductList ({jewelries}) {
                 image={jewelry.image}
                 category={jewelry.category}
                 />
-                
                 //displayed elements as lists first
                 //used the product card component finl
             ))}
