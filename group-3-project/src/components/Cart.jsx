c2051d6a19948af41331c1ff5e661cb78b51d1ef
shantelle wambui <shantellewambuikungu@gmail.com>
2025-07-22 22:14:00 +0300

    feat: create Cart component file and import React

---
+++ group-3-project/src/components/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Cart({items, onRemove }){
+    return (
+        <section className="cart-section">
+            <h2>Cart</h2>
+        </section>
+    )
+}
+export default Cart;
