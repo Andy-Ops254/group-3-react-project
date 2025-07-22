99ecebfe74eff00bb648a85bc3f115002ee0879e
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-22 12:35:14 +0300

    import state and useEffect on App

--- group-3-project/src/App.jsx
+++ group-3-project/src/App.jsx
@@ -1,11 +1,17 @@
-import react from 'react'
+import react,{useState, useEffect} from 'react'
 import jewelryData from '../jewelryData'
 function App() {
   // console.log(jewelryData) JSON DATA IS WORKING Guys!!
+const [jewelryList, setJewelryList]=useState([]) //calling state
+
+useEffect(()=>  {
+  fetch()
+})
+
 
   return (
     <>
-    <h1>TESTING</h1>
+    <h1></h1>
     </>
   )
 }
