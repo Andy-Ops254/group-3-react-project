95b8566a2a7f828993ed05adbab73a33ca886372
shantelle wambui <shantellewambuikungu@gmail.com>
2025-07-22 22:24:31 +0300

    feat: add logic to calculate total cart price using reduce

--- group-3-project/src/components/Cart.jsx
+++ group-3-project/src/components/Cart.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Cart({items, onRemove }){
+    const total =items.reduce((sum, item) => sum + item.price, 0);
+    //first i calculate the total price by summing up the price of all items in the cart
+
     return (
         <section className="cart-section">
             <h2>Cart</h2>
