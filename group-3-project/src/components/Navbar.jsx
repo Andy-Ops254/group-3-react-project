425d5fd6214e917866722dca089e31051b20134b
Agostino <agostinoscholes572@gmail.com>
2025-07-22 12:16:40 +0300

    Add and style Navbar and AddProductForm components

--- group-3-project/src/components/Navbar.jsx
+++ group-3-project/src/components/Navbar.jsx
@@ -1,3 +1,13 @@
 function Navbar({cartCount}) {
-    
+    return (
+        <nav style={Styles.nav}>
+            <h1 style={styles.logo}>Jewelry Boutique</h1>
+            <div style={styles.menu}>
+                <a href="" style={styles.link}>Home</a>
+                <a href="" style={styles.link}>
+                    cart <span style={styles.badge}>{cart}</span>
+                </a>
+            </div>
+        </nav>
+    )
 }
