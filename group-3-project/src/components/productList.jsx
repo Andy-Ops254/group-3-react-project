4b43fbed822b812d236c20b8ce373d535d469ea7
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-23 23:43:11 +0300

    feat: dropdown syle with tailwind

--- group-3-project/src/components/productList.jsx
+++ group-3-project/src/components/productList.jsx
@@ -21,20 +21,26 @@ function ProductList ({jewelries, categories}) {
             <h2 className='text-2xl font-bold font-serif italic text-center my-4'>ALL JEWELRY</h2>
             <p className="font-light italic text-md text-center mb-4">"Explore Our Curated Treasures Across The Seven Seas"</p>
         <div>
-        <label>Jewelry Category: </label>
+        <label className='mx-1.5 font-semibold italic text-[14px]' >JEWELRY CATEGORY: </label>
     
         <select
         onChange={handleClickChange}
-        value= {selectCategory}>
+        value= {selectCategory}
+        className='font-medium italic text-[16px] p-2 border-r-gray-100 bg-gray-200 rounded-xl cursor-pointer hover:bg-sky-200 outline-none mb-10'
+        >
           {categories.map((category, index) => (
             <option key={index}
-            value={category}>{category}</option>
+            value={category}
+            className='bg-white shadow-gray-300 outline-none'
+            >
+            {category}
+            </option>
           ))}
         </select>
         </div>
         
 
-            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mt-10'>
             {updatedCategory.map((jewelry)=> (
                 <ProductCard key={jewelry.id} 
                 name={jewelry.name}
