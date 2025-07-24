b15aaa02965c85f588cddb9c6918806bd8715fd7
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-24 15:10:53 +0300

    feat: style footer complete

--- group-3-project/src/components/footer.jsx
+++ group-3-project/src/components/footer.jsx
@@ -3,19 +3,19 @@ import {Mail, Instagram, Phone} from 'lucide-react'
 
 function  Footer () {
   return (
-    <footer className='flex flex-row gap-6 justify-between bg-[#0065F8] text-white'>
-        <div className='font-bold my-5 cursor-pointer hove:bg-'>
-        <p>&copy;2025</p>
+    <footer className='flex flex-row gap-6 justify-between bg-white text-[#b6115b] w-full h-auto rounded-2xl'>
+        <div className='font-bold cursor-pointer'>
+        <p className=''>&copy;2025</p>
         </div>
 
-        <div className='flex'>
+        <div className='flex space-x-4 cursor-pointer my-5 font-bold'>
          <Mail/>
         <Instagram />
         <Phone/>
         </div>
 
-        <div>
-            <h2>Group 3 react app</h2>
+        <div className='font-bold font-serif cursor-pointer p-2'>
+            <h2 className='my-2 underline underline-offset-2'>Group 3 react app</h2>
             <p>Shantell Wambui</p>
             <p>Andrew Rimongi</p>
             <p>Agostino Omare</p>
